c6777ad58b6f77f54802bf4818d42f1691561b8f
DELLINGER Agathe <agathe.dellinger2@etu.univ-lorraine.fr>
2024-01-25 17:25:36 +0100

    api OK et route Activity = /

--- src/main.js
+++ src/main.js
@@ -4,11 +4,13 @@ import piniaPersist from 'pinia-plugin-persist'
 
 import App from './App.vue'
 import router from './router'
+import api from './plugins/api.js'
 
 const app = createApp(App)
 const pinia = createPinia();
 app.use(pinia);
 pinia.use(piniaPersist)
 app.use(router)
+app.use(api)
 
 app.mount('#app')

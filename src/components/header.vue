eaa9e2b1885c748cedbb7c40a6995533600abab9
Antoine Stoecklin <astoeck@hotmail.fr>
2024-01-25 15:12:42 +0100

    new name and header created

---
+++ src/components/header.vue
@@ -0,0 +1,24 @@
+<template>
+ <nav>
+    <div class="upperNav">
+        <p>nombres d'heures travaillées aujourd'hui: {{ totalCount }}</p>
+        <p>nombres d'objectifs atteints: {{ totalObjectives }}</p>
+        <routerLink to="../views/Activity.vue">Voir les activités</routerLink>
+        <routerLink to="../views/Project.vue">Voir les statistiques</routerLink>
+    </div>
+    <div class="bottomNav">
+        <p>Activités en cours: {{ PendingActivity }}</p>
+        <button type="button" aria-label="stop"><img src="../assets/img/Stop_button.png"></button>
+        <routerLink to="../views/Settings.vue">Voir les activités</routerLink>
+        <routerLink to="../views/Project.vue">Voir les statistiques</routerLink>
+    </div>
+ </nav>
+</template>
+
+<script>
+
+</script>
+
+<style>
+
+</style>

3385c2777cc53ba4f0f076f9a494fd57cdaa5c30
Antoine Stoecklin <astoeck@hotmail.fr>
2024-01-25 16:41:03 +0100

    started router

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Home from "@/views/Home.vue";
-import Activities from "@/views/Activities.vue";
+import Activity from "@/views/Activities.vue";
+import Project from "@/views/Project.vue"
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -11,8 +12,15 @@ const router = createRouter({
     },
     {
       path: '/activities',
-      component : Activities
+      component : Activity
     },
+    {
+      path:'/projects',
+      component: Project
+    },
+    {
+      path:''
+    }
   ]
 })
 

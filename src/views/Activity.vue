a7dfcbdda6b5dbfd764ae63be37a4eb6666fd991
DELLINGER Agathe <agathe.dellinger2@etu.univ-lorraine.fr>
2024-01-25 16:48:14 +0100

    activity(accueil) (view + component)

---
+++ src/views/Activity.vue
@@ -0,0 +1,44 @@
+<script>
+import { RouterLink, RouterView } from 'vue-router'
+import Activity from '@/components/Activity.vue'
+
+export default {
+  components: {
+    RouterLink,
+    Activity
+  },
+  data() {
+    return {
+      activities: []
+    }
+  },
+  methods: {
+  },
+  directives : {
+    color: {
+      mounted(el, binding) {
+        el.style.color = binding.value;
+      }
+    }
+  },
+  created() {
+    this.$api.get('activities').then((resp) => {
+      console.log(resp.data)
+      this.activities = resp.data
+    })
+  },
+}
+</script>
+
+<template>
+  <div class="activities">
+    <div class="activity" v-for="activity in activities" :key="activity.id">
+      <Activity v-color="activity.color" :name="activity.name"/>
+    </div>
+  </div>
+
+</template>
+
+<style scoped>
+
+</style>

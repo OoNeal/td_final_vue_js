67cdfe606cdc1f1e213378b21f3c2def4d9a8d5d
DELLINGER Agathe <agathe.dellinger2@etu.univ-lorraine.fr>
2024-01-25 16:37:44 +0100

    api+activities in router

--- src/plugins/api.js
+++ src/plugins/api.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 export default {
     install: function (app, options) {
         app.config.globalProperties.$api = axios.create({
-            baseURL : 'https://donations.edu.netlor.fr/api/',
+            baseURL : 'https://timely.edu.netlor.fr/api/',
             headers: {
                 'Content-Type': 'application/json',
                 'Authorization': 'key=rcMY3y*B,9Vj'
--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,17 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import Home from "@/views/Home.vue";
+import Activities from "@/views/Activities.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
-      name: 'home',
+      component : Home
+    },
+    {
+      path: '/activities',
+      component : Activities
     },
   ]
 })

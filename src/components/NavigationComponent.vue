59ea922a412e93d2ec33064da0fceb594638573a
OoNeal <arnaud35u@etu.univ-lorraine.fr>
2024-01-27 15:03:17 +0100

    EsLint Correction

--- src/components/NavigationComponent.vue
+++ src/components/NavigationComponent.vue
@@ -1,73 +1,73 @@
 <template>
- <nav>
+  <nav>
     <div class="upperNav">
-        <button @click="toggleTimer">Commencer son temps de travail:   {{ formatTime(duration) }}   </button>
-        <p>nombres d'objectifs atteints:  </p>
-        <routerLink to="../views/Activity.vue">Voir les activités</routerLink>
-        <routerLink to="../views/Project.vue">Voir les statistiques</routerLink>
+      <button @click="toggleTimer">Commencer son temps de travail: {{ formatTime(duration) }}</button>
+      <p>nombres d'objectifs atteints: </p>
+      <routerLink to="/activity">Voir les activités</routerLink>
+      <routerLink to="/project">Voir les statistiques</routerLink>
 
     </div>
 
     <div class="bottomNav">
 
-        <p>Activités en cours: </p>
-        <button type="button" aria-label="stop"><img src="../assets/img/Stop_button.png">Stopper l'activité en cours</button>
-        <routerLink to="../views/Settings.vue">Paramètres globaux </routerLink>
-        <routerLink to="../views/Settings.vue">Voir les statistiques</routerLink>
+      <p>Activités en cours: </p>
+      <button type="button" aria-label="stop"><img src="../assets/img/Stop_button.png">Stopper l'activité en cours
+      </button>
+      <routerLink to="/setting">Paramètres globaux</routerLink>
+      <routerLink to="/setting">Voir les statistiques</routerLink>
 
     </div>
- </nav>
+  </nav>
 </template>
 
 <script>
-import api from '../plugins/api.js'
 
 
-export default{
-    data() {
+export default {
+  data() {
     return {
-        timerRunning: false,
-        startTime: null,
-        duration: 0,
-        timerInterval: null,
-        
-    };
-    },
-    methods: {
+      timerRunning: false,
+      startTime: null,
+      duration: 0,
+      timerInterval: null
+
+    }
+  },
+  methods: {
     toggleTimer() {
       if (this.timerRunning) {
-        this.stopTimer();
+        this.stopTimer()
       } else {
-        this.startTimer();
+        this.startTimer()
       }
     },
     startTimer() {
-      this.timerRunning = true;
-      this.startTime = Date.now();
-      this.timerInterval = setInterval(function () {
-        this.duration = Date.now() - this.startTime;
-      }.bind(this), 1000);
+      this.timerRunning = true
+      this.startTime = Date.now()
+      this.timerInterval = setInterval(function() {
+        this.duration = Date.now() - this.startTime
+      }.bind(this), 1000)
     },
     stopTimer() {
-      this.timerRunning = false;
-      clearInterval(this.timerInterval);
+      this.timerRunning = false
+      clearInterval(this.timerInterval)
     },
     formatTime(milliseconds) {
-      const seconds = Math.floor(milliseconds / 1000);
-      const minutes = Math.floor(seconds / 60);
-      const hours = Math.floor(minutes / 60);
+      const seconds = Math.floor(milliseconds / 1000)
+      const minutes = Math.floor(seconds / 60)
+      const hours = Math.floor(minutes / 60)
 
       const formattedTime =
         String(hours).padStart(2, '0') +
         ':' +
         String(minutes % 60).padStart(2, '0') +
         ':' +
-        String(seconds % 60).padStart(2, '0');
+        String(seconds % 60).padStart(2, '0')
 
-      return formattedTime;
-    },
-  
-  },
+      return formattedTime
+    }
+
+  }
 }
 
 </script>
@@ -75,98 +75,98 @@ export default{
 <style scoped>
 @import url('https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap');
 
-p{
-    color: #FFFFFF ;
+p {
+  color: #FFFFFF;
 }
 
 nav {
-    display: flex;
-    flex-direction: column;
-   
+  display: flex;
+  flex-direction: column;
+
 }
 
 
 .upperNav {
-    display: flex;
-    justify-content: space-around;
-    align-items: center;
-    background-color:#02020A;
-    height:50px;
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  background-color: #02020A;
+  height: 50px;
 }
 
 .upperNav p {
-    margin: 0; 
-    font-family:'Poppins',sans-serif;
-    font-weight:300;
-    font-size:20px;
+  margin: 0;
+  font-family: 'Poppins', sans-serif;
+  font-weight: 300;
+  font-size: 20px;
 }
 
 
 .upperNav a {
-    font-family:'Poppins',sans-serif;
-    font-weight:300;
-    font-size:20px;
-    text-decoration: none;
-    color: #FFFFFF; 
-    margin-left: 15px; 
+  font-family: 'Poppins', sans-serif;
+  font-weight: 300;
+  font-size: 20px;
+  text-decoration: none;
+  color: #FFFFFF;
+  margin-left: 15px;
 }
 
 .upperNav button {
-    font-family:'Poppins', sans-serif;
-    background-color: #ffffff; 
-    border: none;
-    padding: 10px;
-    cursor: pointer;
-    border-radius: 20px;
-    text-transform:uppercase;
+  font-family: 'Poppins', sans-serif;
+  background-color: #ffffff;
+  border: none;
+  padding: 10px;
+  cursor: pointer;
+  border-radius: 20px;
+  text-transform: uppercase;
 }
 
 
 .bottomNav {
-    display: flex;
-    justify-content:space-around;
-    align-items: center;
-    background-color:#E1E2EF;
-    height:100px;
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  background-color: #E1E2EF;
+  height: 100px;
 }
 
 .bottomNav p {
-    margin: 0;
-    font-family:'Poppins',sans-serif;
-    font-weight:300;
-    font-size:20px;
-    color: #333 ;
+  margin: 0;
+  font-family: 'Poppins', sans-serif;
+  font-weight: 300;
+  font-size: 20px;
+  color: #333;
 }
 
 
 .bottomNav button {
-    font-family:'Poppins', sans-serif;
-    background-color: #333; 
-    color: #fff;
-    border: none;
-    padding: 10px;
-    cursor: pointer;
-    border-radius: 20px;
-    text-align:center;
-    vertical-align:middle;
+  font-family: 'Poppins', sans-serif;
+  background-color: #333;
+  color: #fff;
+  border: none;
+  padding: 10px;
+  cursor: pointer;
+  border-radius: 20px;
+  text-align: center;
+  vertical-align: middle;
 }
 
 
 .bottomNav a {
-    text-decoration: none;
-    color: #333 ;   
-     margin-left: 15px; 
-    font-family:'Poppins',sans-serif;
-    font-weight:300;
-    font-size:20px;
+  text-decoration: none;
+  color: #333;
+  margin-left: 15px;
+  font-family: 'Poppins', sans-serif;
+  font-weight: 300;
+  font-size: 20px;
 }
 
-img{
-    width: 25px;
-    height:25px;
-    text-align:center;
-    vertical-align:middle;
-    padding:2px;
+img {
+  width: 25px;
+  height: 25px;
+  text-align: center;
+  vertical-align: middle;
+  padding: 2px;
 }
 
 </style>

23bcedc34b31150c17cb66e21a390afcd7b37ee9
DELLINGER Agathe <agathe.dellinger2@etu.univ-lorraine.fr>
2024-03-08 20:29:17 +0100

    refactor ActivityView en HomeView

--- src/router/index.js
+++ src/router/index.js
@@ -1,13 +1,12 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import { useAuthStore } from '@/stores/Auth.js'
-import Activity from "@/views/ActivityView.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path:'/',
-      component: Activity
+      component: () => import('@/views/HomeView.vue'),
     },
     {
       path: '/auth',

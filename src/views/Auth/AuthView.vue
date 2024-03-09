eaefdece5c5f801064ea9637553aa771df4e44f4
OoNeal <arnaud35u@etu.univ-lorraine.fr>
2024-03-09 02:29:40 +0100

    Add authentification style

--- src/views/Auth/AuthView.vue
+++ src/views/Auth/AuthView.vue
@@ -3,9 +3,25 @@
 </script>
 
 <template>
-<RouterView/>
+  <main>
+    <img src="/public/img/hourglass.jpg" alt="B&W hourglass">
+    <RouterView />
+  </main>
 </template>
 
-<style scoped>
+<style scoped lang='scss'>
+  main {
+    width: 100vw;
+    height: calc(100vh - 68px);
 
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+
+    & > img {
+      width: 40%;
+      height: 100%;
+      object-fit: cover;
+    }
+  }
 </style>
--- src/views/Auth/LoginView.vue
+++ src/views/Auth/LoginView.vue
@@ -37,14 +37,101 @@ export default {
 </script>
 
 <template>
-  <div>
-    <h2>Login Page</h2>
-    <input v-model="userKey">
-    <button @click="login">Login</button>
-    <RouterLink to="/auth/register">Register</RouterLink>
+  <div class="login">
+    <h2>Se connecter</h2>
+    <div class="login_form">
+      <h3>Clé d'API</h3>
+      <input v-model="userKey">
+      <div class="login_form_buttons">
+        <RouterLink to="/auth/register">S'inscrire</RouterLink>
+        <button @click="login">Se connecter</button>
+      </div>
+    </div>
   </div>
 </template>
 
-<style scoped>
+<style scoped lang="scss">
+.login {
+  width: 60%;
+  height: 100%;
 
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+
+  & > h2 {
+    font-size: 3em;
+    font-weight: 300;
+    text-transform: uppercase;
+  }
+
+  &_form {
+    width: 50%;
+    margin-top: 1em;
+
+    display: flex;
+    flex-direction: column;
+    align-items: start;
+
+    & > h3 {
+      margin: .5em 0;
+      color: #D4DFD8;
+      font-size: 1.2em;
+      font-weight: 500;
+      text-transform: uppercase;
+    }
+
+    & > input {
+      box-sizing: border-box;
+
+      margin-bottom: 1em;
+
+      width: 100%;
+      padding: .5em 1em;
+
+      border: 1px solid rgba(#D4DFD8, 0.35);
+      border-radius: 100px;
+      background-color: rgba(#D4DFD8, 0.2);
+
+      font-family: 'Montserrat', sans-serif;
+      color: #D4DFD8;
+      font-size: 1em;
+      font-weight: 400;
+
+      &::placeholder {
+        text-transform: uppercase;
+      }
+    }
+
+    &_buttons {
+      width: 100%;
+
+      display: flex;
+      flex-direction: row;
+      justify-content: space-between;
+      align-items: baseline;
+
+      & > button {
+        box-sizing: border-box;
+        padding: 0.5em 1em;
+        border: 1px solid rgba(#D4DFD8, 0.35);
+        border-radius: 100px;
+        background-color: rgba(#D4DFD8, 0.2);
+        color: #D4DFD8;
+
+        font-size: 1em;
+        font-weight: 500;
+      }
+
+      & > a {
+        color: #D4DFD8;
+        font-size: 1em;
+        font-weight: 500;
+        text-decoration: none;
+      }
+    }
+
+  }
+}
 </style>
